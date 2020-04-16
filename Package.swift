3622467a693bba27bfa2f05993a12798e3dd1715
Jan Scheithauer <j.scheithauer@gmail.com>
2020-04-17 01:56:17 +0200

    Add swift-nio dependency

--- Package.swift
+++ Package.swift
@@ -6,15 +6,17 @@ import PackageDescription
 let package = Package(
     name: "swift_nio_example",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift_nio_example",
-            dependencies: []),
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio")
+            ]),
         .testTarget(
             name: "swift_nio_exampleTests",
             dependencies: ["swift_nio_example"]),

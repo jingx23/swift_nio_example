3c23fe55133891936fa2727fe50deba7c2816770
Jan Scheithauer <j.scheithauer@gmail.com>
2020-04-17 01:20:10 +0200

    swift package init --type executable

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift_nio_example
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_nio_exampleTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_nio_exampleTests.allTests()
+XCTMain(tests)

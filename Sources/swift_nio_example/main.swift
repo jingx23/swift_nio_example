d095dfd2968dd63181ba5fd0eb7ca2d6a698b214
Jan Scheithauer <j.scheithauer@gmail.com>
2020-04-17 03:05:38 +0200

    Read GET Parameter and print hello world

--- Sources/swift_nio_example/main.swift
+++ Sources/swift_nio_example/main.swift
@@ -1,1 +1,243 @@
-print("Hello, world!")
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+import NIO
+import NIOHTTP1
+import Foundation
+
+extension String {
+    func chopPrefix(_ prefix: String) -> String? {
+        if self.unicodeScalars.starts(with: prefix.unicodeScalars) {
+            return String(self[self.index(self.startIndex, offsetBy: prefix.count)...])
+        } else {
+            return nil
+        }
+    }
+}
+
+private func httpResponseHead(request: HTTPRequestHead, status: HTTPResponseStatus, headers: HTTPHeaders = HTTPHeaders()) -> HTTPResponseHead {
+    var head = HTTPResponseHead(version: request.version, status: status, headers: headers)
+    let connectionHeaders: [String] = head.headers[canonicalForm: "connection"].map { $0.lowercased() }
+
+    if !connectionHeaders.contains("keep-alive") && !connectionHeaders.contains("close") {
+        // the user hasn't pre-set either 'keep-alive' or 'close', so we might need to add headers
+
+        switch (request.isKeepAlive, request.version.major, request.version.minor) {
+        case (true, 1, 0):
+            // HTTP/1.0 and the request has 'Connection: keep-alive', we should mirror that
+            head.headers.add(name: "Connection", value: "keep-alive")
+        case (false, 1, let n) where n >= 1:
+            // HTTP/1.1 (or treated as such) and the request has 'Connection: close', we should mirror that
+            head.headers.add(name: "Connection", value: "close")
+        default:
+            // we should match the default or are dealing with some HTTP that we don't support, let's leave as is
+            ()
+        }
+    }
+    return head
+}
+
+private final class HTTPHandler: ChannelInboundHandler {
+    public typealias InboundIn = HTTPServerRequestPart
+    public typealias OutboundOut = HTTPServerResponsePart
+
+    private enum State {
+        case idle
+        case waitingForRequestBody
+        case sendingResponse
+
+        mutating func requestReceived() {
+            precondition(self == .idle, "Invalid state for request received: \(self)")
+            self = .waitingForRequestBody
+        }
+
+        mutating func requestComplete() {
+            precondition(self == .waitingForRequestBody, "Invalid state for request complete: \(self)")
+            self = .sendingResponse
+        }
+
+        mutating func responseComplete() {
+            precondition(self == .sendingResponse, "Invalid state for response complete: \(self)")
+            self = .idle
+        }
+    }
+
+    private var buffer: ByteBuffer! = nil
+    private var keepAlive = false
+    private var state = State.idle
+
+    private func completeResponse(_ context: ChannelHandlerContext, trailers: HTTPHeaders?, promise: EventLoopPromise<Void>?) {
+        self.state.responseComplete()
+
+        let promise = self.keepAlive ? promise : (promise ?? context.eventLoop.makePromise())
+        if !self.keepAlive {
+            promise!.futureResult.whenComplete { (_: Result<Void, Error>) in context.close(promise: nil) }
+        }
+
+        context.writeAndFlush(self.wrapOutboundOut(.end(trailers)), promise: promise)
+    }
+
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        let reqPart = self.unwrapInboundIn(data)
+
+        switch reqPart {
+        case .head(let request):
+            var echo = "Hello World\r\n"
+            if let urlComponents = URLComponents(string: request.uri) {
+                echo = echo + "Path: \(urlComponents.path)\r\n"
+                urlComponents.queryItems?.forEach { item in
+                    echo = echo + "\(item.name) = \(item.value ?? "")\r\n"
+                }
+            }
+            self.keepAlive = request.isKeepAlive
+            self.state.requestReceived()
+
+            var responseHead = httpResponseHead(request: request, status: HTTPResponseStatus.ok)
+            self.buffer.clear()
+            self.buffer.writeString(echo)
+            responseHead.headers.add(name: "content-length", value: "\(self.buffer!.readableBytes)")
+            let response = HTTPServerResponsePart.head(responseHead)
+            context.write(self.wrapOutboundOut(response), promise: nil)
+        case .body:
+            break
+        case .end:
+            self.state.requestComplete()
+            let content = HTTPServerResponsePart.body(.byteBuffer(buffer!.slice()))
+            context.write(self.wrapOutboundOut(content), promise: nil)
+            self.completeResponse(context, trailers: nil, promise: nil)
+        }
+    }
+
+    func channelReadComplete(context: ChannelHandlerContext) {
+        context.flush()
+    }
+
+    func handlerAdded(context: ChannelHandlerContext) {
+        self.buffer = context.channel.allocator.buffer(capacity: 0)
+    }
+
+    func userInboundEventTriggered(context: ChannelHandlerContext, event: Any) {
+        switch event {
+        case let evt as ChannelEvent where evt == ChannelEvent.inputClosed:
+            // The remote peer half-closed the channel. At this time, any
+            // outstanding response will now get the channel closed, and
+            // if we are idle or waiting for a request body to finish we
+            // will close the channel immediately.
+            switch self.state {
+            case .idle, .waitingForRequestBody:
+                context.close(promise: nil)
+            case .sendingResponse:
+                self.keepAlive = false
+            }
+        default:
+            context.fireUserInboundEventTriggered(event)
+        }
+    }
+}
+
+// First argument is the program path
+var arguments = CommandLine.arguments.dropFirst(0) // just to get an ArraySlice<String> from [String]
+var allowHalfClosure = true
+if arguments.dropFirst().first == .some("--disable-half-closure") {
+    allowHalfClosure = false
+    arguments = arguments.dropFirst()
+}
+let arg1 = arguments.dropFirst().first
+let arg2 = arguments.dropFirst(2).first
+
+let defaultHost = "::1"
+let defaultPort = 8888
+
+enum BindTo {
+    case ip(host: String, port: Int)
+    case unixDomainSocket(path: String)
+    case stdio
+}
+
+let bindTarget: BindTo
+
+switch (arg1, arg1.flatMap(Int.init), arg2, arg2.flatMap(Int.init)) {
+case (.some(let h), _ , _, .some(let p)):
+    /* second arg an integer --> host port [htdocs] */
+    bindTarget = .ip(host: h, port: p)
+case (_, .some(let p), _, _):
+    /* first arg an integer --> port [htdocs] */
+    bindTarget = .ip(host: defaultHost, port: p)
+case (.some(let portString), .none, .none, .none):
+    /* couldn't parse as number --> uds-path-or-stdio [htdocs] */
+    if portString == "-" {
+        bindTarget = .stdio
+    } else {
+        bindTarget = .unixDomainSocket(path: portString)
+    }
+default:
+    bindTarget = BindTo.ip(host: defaultHost, port: defaultPort)
+}
+
+let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+
+func childChannelInitializer(channel: Channel) -> EventLoopFuture<Void> {
+    return channel.pipeline.configureHTTPServerPipeline(withErrorHandling: true).flatMap {
+        channel.pipeline.addHandler(HTTPHandler())
+    }
+}
+
+let socketBootstrap = ServerBootstrap(group: group)
+    // Specify backlog and enable SO_REUSEADDR for the server itself
+    .serverChannelOption(ChannelOptions.backlog, value: 256)
+    .serverChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+    
+    // Set the handlers that are applied to the accepted Channels
+    .childChannelInitializer(childChannelInitializer(channel:))
+
+    // Enable SO_REUSEADDR for the accepted Channels
+    .childChannelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
+    .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 1)
+    .childChannelOption(ChannelOptions.allowRemoteHalfClosure, value: allowHalfClosure)
+let pipeBootstrap = NIOPipeBootstrap(group: group)
+    // Set the handlers that are applied to the accepted Channels
+    .channelInitializer(childChannelInitializer(channel:))
+    
+    .channelOption(ChannelOptions.maxMessagesPerRead, value: 1)
+    .channelOption(ChannelOptions.allowRemoteHalfClosure, value: allowHalfClosure)
+
+defer {
+    try! group.syncShutdownGracefully()
+}
+
+let channel = try { () -> Channel in
+    switch bindTarget {
+    case .ip(let host, let port):
+        return try socketBootstrap.bind(host: host, port: port).wait()
+    case .unixDomainSocket(let path):
+        return try socketBootstrap.bind(unixDomainSocketPath: path).wait()
+    case .stdio:
+        return try pipeBootstrap.withPipes(inputDescriptor: STDIN_FILENO, outputDescriptor: STDOUT_FILENO).wait()
+    }
+}()
+
+let localAddress: String
+if case .stdio = bindTarget {
+    localAddress = "STDIO"
+} else {
+    guard let channelLocalAddress = channel.localAddress else {
+        fatalError("Address was unable to bind. Please check that the socket was not closed or that the address family was understood.")
+    }
+    localAddress = "\(channelLocalAddress)"
+}
+print("Server started and listening on \(localAddress)")
+
+// This will never unblock as we don't close the ServerChannel
+try channel.closeFuture.wait()
+
+print("Server closed")
